e2c6bf30ad90ad7b0662c7b5c31120574690898e
agalindo2014 <aprilgalindo2014@gmail.com>
2023-05-05 00:12:54 -0500

    update

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,24 @@
 // Assignment code here
+var confirmSymbol;
+var confirmNumber;
+var confirmUppercase;
+var confirmLowercase;
+var confirmAlpha;
+var enter;
+
+
+// Password Variable Values
+// Numeric
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Alphabetic
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Symbols
+symbol = ["!", "@", "#", "$", "%", "^", "&", "*"];
+space = [];
+
+// Choices outside of statement for concantation for condition
+var choices;
+
 
 
 // Get references to the #generate element
@@ -11,7 +31,37 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// switches to uppercase
+var toUpper = function (x) {
+  return x.toUpperCase();
+};
+
+var get = document.querySelector("generate");
+
+get.addEventListener("click", function ()) {
+  ps = generatePassword();
+  document.getElementById("password").placeholder = ps;
+};
+
+// password generation start
+function generatePassword() {
+  enter = parseInt(prompt("Please input 8-128 characters"));
+if (!enter) {
+  alert("Please enter value");
+}
+else if (enter < 8 || enter > 128) {
+  enter = parseInt(prompt("There must be 8-128 characters"));
+}
+} else {
+  confirmUppercase = confirm("Does this have Uppercase values?");
+  confirmLowercase = confirm("Does this have Lowercase values?");
+  confirmNumber = confirm("Does this have number values?");
+  confirmSymbol = confirm("does this have symbols?");
+};
