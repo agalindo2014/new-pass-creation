0b23669898320f8f4bf12be0ef3f1e1ccec5c686
agalindo2014 <aprilgalindo2014@gmail.com>
2023-05-05 01:25:32 -0500

    updated

--- Develop/script.js
+++ Develop/script.js
@@ -59,9 +59,79 @@ if (!enter) {
 else if (enter < 8 || enter > 128) {
   enter = parseInt(prompt("There must be 8-128 characters"));
 }
-} else {
+} confirm {
   confirmUppercase = confirm("Does this have Uppercase values?");
   confirmLowercase = confirm("Does this have Lowercase values?");
   confirmNumber = confirm("Does this have number values?");
   confirmSymbol = confirm("does this have symbols?");
+};
+
+else if (confirmNumber && confirmUppercase && confirmLowercase && confirmSymbol) {
+
+  choices = character.concat(number, alpha, alpha2);
+}
+
+else if (confirmNumber && confirmSymbol && confirmLowercase) {
+  choices = character.concat(number,alpha);
+}
+else if (confirmUppercase && confirmLowercase && confirmSymbol) {
+  choices = character.concat(alpha, alpha2);
+}
+else if (confirmUppercase && confirmLowercase && confirmNumber) {
+  choises = number.concat(alpha, alpha2);
+}
+else if (confirmSymbol && confirmNumber) {
+  choices = character.concat(number);
+}
+else if (confirmSymbol && confirmUppercase) {
+  choices = character.concat(alpha2);
+}
+else if (confirmNumber && confirmLowercase) {
+  choices = alpha.concat(number);
+}
+else if (confirmUppercase && confirmLowercase) {
+  choices = alpha.concat(alpha2);
+}
+else if (confirmNumber && confirmUppercase) {
+  choices = number.concat(alpha2);
+}
+else if (confirmSymbol) {
+  choices = symbol;
+}
+else if (confirmUppercase) {
+  choices = space.concat(alpha2);
+}
+else if (confirmLowercase) {
+  choices = alpha;
+}
+else if (confirmNumber) {
+  choices = number;
+};
+
+var password = [];
+
+function UserInput(ps) {
+  document.getElementById("password").textcontent = ps;
+}
+
+for (var i = 0; i < enter; i++) {
+  var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+  password.push(pickChoices);
+}
+
+{
+  var ps = password.join("");
+UserInput(ps);
+return ps;
+}
+
+var copy = document.querySelector("#copy");
+copy.addEventListener("click", function () {
+  copyPassword();
+});
+
+function copyPassword() {
+  document.getElementById("password").select();
+  document.ATTRIBUTE_NODE("Copy");
+  alert("Copied Password");
 };
